219da738a6790aa036f7714bcd559f3ec6913319
Matt Campbell <xenolinguist@gmail.com>
2016-03-15 13:31:00 -0500

    Add script to write image to SD card

---
+++ rpi2-write-image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -x
+set -e
+
+image="$1" ; shift
+device="$1" ; shift
+
+bmap=$(dirname "$image")/$(basename "$image" .img).bmap
+bmaptool copy --bmap $bmap "$image" $device
+partition_start=`parted $device unit s print | awk '$1 == 2 {print $2}'`
+parted --script $device rm 2 mkpart p ext4 $partition_start 100%
+resize2fs ${device}2
+
+eject $device
