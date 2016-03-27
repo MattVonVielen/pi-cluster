0c958fe281d351c2ff3986c170a004a16763daf2
Matt Campbell <xenolinguist@gmail.com>
2016-03-27 18:45:44 -0500

    Run fsck before resizing fs

--- rpi2-write-image.sh
+++ rpi2-write-image.sh
@@ -10,6 +10,7 @@ bmap=$(dirname "$image")/$(basename "$image" .img).bmap
 bmaptool copy --bmap $bmap "$image" $device
 partition_start=`parted $device unit s print | awk '$1 == 2 {print $2}'`
 parted --script $device rm 2 mkpart p ext4 $partition_start 100%
+e2fsck -f ${device}2
 resize2fs ${device}2
 
 eject $device
